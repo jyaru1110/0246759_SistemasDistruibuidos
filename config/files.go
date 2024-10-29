b017931e8f938cb352cdec0b31c6357817ec5fee
Jyaru Hernandez <jyaru1110@gmail.com>
2024-10-29 16:50:52 -0600

    security: tls go files

---
+++ config/files.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+var (
+	CAFile               = configFile("ca.pem")
+	ServerCertFile       = configFile("server.pem")
+	ServerKeyFile        = configFile("server-key.pem")
+	RootClientCertFile   = configFile("root-client.pem")
+	RootClientKeyFile    = configFile("root-client-key.pem")
+	NobodyClientCertFile = configFile("nobody-client.pem")
+	NobodyClientKeyFile  = configFile("nobody-client-key.pem")
+	ACLModelFile         = configFile("model.conf")
+	ACLPolicyFile        = configFile("policy.csv")
+)
+
+func configFile(filename string) string {
+	dir := os.Getenv("CONFIG_DIR")
+	return filepath.Join(dir, filename)
+}
