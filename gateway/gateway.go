03f50b1d6deb968127df4b01d18ca05dc436bba4
Jyaru Hernandez <jyaru1110@gmail.com>
2024-11-03 22:50:45 -0600

    feat: gateway credentials

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -22,25 +22,30 @@ var addr_todo = flag.String("addr_todo", "todoservice:8081", "the address to con
 var addr_log = flag.String("addr_log", "logservice:8080", "the address to connect to")
 
 func main() {
-	//Tood service connection
-	todo_conn, err := grpc.NewClient(*addr_todo, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Load client TLS credentials
+	clientTLSConfig, err := tlsconfig.SetupTLSConfig(tlsconfig.TLSConfig{
+		CertFile: tlsconfig.NobodyClientCertFile,
+		KeyFile:  tlsconfig.NobodyClientKeyFile,
+		CAFile:   tlsconfig.CAFile,
+		Server:   false,
+	})
+
+	if err != nil {
+		fmt.Printf("could set client creds config: %v", err)
+		return
+	}
+
+	clientCreds := credentials.NewTLS(clientTLSConfig)
+
+	log_conn, err := grpc.NewClient(*addr_log, grpc.WithTransportCredentials(clientCreds))
+
 	if err != nil {
 		fmt.Printf("could not connect to todo grpcserver: %v", err)
 		return
 	}
-	// Load client TLS credentials
-	clientTLSConfig, err := tlsconfig.SetupTLSConfig(tlsconfig.TLSConfig{
-		CertFile: tlsconfig.ClientCertFile,
-		KeyFile:  tlsconfig.ClientKeyFile,
-		CAFile:   tlsconfig.CAFile,
-	})
-	if err != nil {
-		fmt.Printf("could not load tls config: %v", err)
-		return
-	}
-	clientCreds := credentials.NewTLS(clientTLSConfig)
-	// Log service connection
-	log_conn, err := grpc.NewClient(*addr_log, grpc.WithTransportCredentials(clientCreds))
+
+	//todo service connection
+	todo_conn, err := grpc.NewClient(*addr_todo, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		fmt.Printf("could not connect to log grpcserver: %v", err)
@@ -78,6 +83,8 @@ func main() {
 	rest_server.HandleFunc("/todo/{id}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id := vars["id"]
+
+		fmt.Println("trying to get todo by id: ", id)
 
 		log_response, error := log_grpc_client.Produce(ctx, &api.ProduceRequest{Record: &api.Record{Value: []byte("Get todo by id: " + id)}})
 
